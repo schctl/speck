57d34bc6f5c651841a375e535f66978aa056b7f6
Sachin Cherian <sachinctl@protonmail.com>
2021-04-10 18:48:34 +0530

    cleanup imports and exception

--- speck_ui/__init__.py
+++ speck_ui/__init__.py
@@ -7,4 +7,4 @@ Licensed under the MIT license, see LICENSE for more information.
 __authors__ = ['Nevin Jose', 'Sachin Cherian']
 
 from . import calculator
-from .style import *
+from . import style
