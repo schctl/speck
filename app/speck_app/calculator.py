78790d15abf9785b57b3f3551c17e1b5a2fc203c
Sachin Cherian <sachinctl@protonmail.com>
2021-04-29 23:25:25 +0530

    fix calculator impl

--- app/speck_app/calculator.py
+++ app/speck_app/calculator.py
@@ -14,7 +14,7 @@ class Calculator:
         # stores what operation to perform
         # on the last stored value and
         # incoming value
-        self._math = ""
+        self._math = ''
         # stored value
         self._first_num = 1
 
@@ -38,9 +38,9 @@ class Calculator:
             e.delete(0, tk.END)
 
         def button_click(number):
+            prev = e.get()
             button_clear()
-
-            e.insert(0, str(e.get()) + str(number))
+            e.insert(0, str(prev) + str(number))
 
         def button_add():
             self._math = 'add'
@@ -122,30 +122,27 @@ class Calculator:
             elif self._math == 'floord':
                 e.insert(0, self._first_num // int(second_num))
 
+            self._math = ''
+
         # -------------------------------------------------
 
         tk.Button(root, text="1",     padx=40, pady=20, command=lambda: button_click(1)) .grid(row=4, column=0, columnspan=1)
         tk.Button(root, text="2",     padx=40, pady=20, command=lambda: button_click(2)) .grid(row=4, column=1, columnspan=1)
         tk.Button(root, text="3",     padx=40, pady=20, command=lambda: button_click(3)) .grid(row=4, column=2, columnspan=1)
-
         tk.Button(root, text="4",     padx=40, pady=20, command=lambda: button_click(4)) .grid(row=3, column=0, columnspan=1)
         tk.Button(root, text="5",     padx=40, pady=20, command=lambda: button_click(5)) .grid(row=3, column=1, columnspan=1)
         tk.Button(root, text="6",     padx=40, pady=20, command=lambda: button_click(6)) .grid(row=3, column=2, columnspan=1)
-
         tk.Button(root, text="7",     padx=40, pady=20, command=lambda: button_click(7)) .grid(row=2, column=0, columnspan=1)
         tk.Button(root, text="8",     padx=40, pady=20, command=lambda: button_click(8)) .grid(row=2, column=1, columnspan=1)
         tk.Button(root, text="9",     padx=40, pady=20, command=lambda: button_click(9)) .grid(row=2, column=2, columnspan=1)
-
         tk.Button(root, text="0",     padx=40, pady=20, command=lambda: button_click(0)) .grid(row=5, column=1, columnspan=1)
 
         tk.Button(root, text="+",     padx=40, pady=20, command=button_add)              .grid(row=5, column=3, columnspan=1)
         tk.Button(root, text="=",     padx=39, pady=20, command=button_equal)            .grid(row=5, column=2, columnspan=1)
         tk.Button(root, text="CLEAR", padx=26, pady=20, command=button_clear)            .grid(row=5, column=0, columnspan=1)
-
         tk.Button(root, text="-",     padx=40, pady=20, command=button_sub)              .grid(row=4, column=3, columnspan=1)
         tk.Button(root, text="x",     padx=40, pady=20, command=button_multiply)         .grid(row=3, column=3, columnspan=1)
         tk.Button(root, text="/",     padx=40, pady=20, command=button_div)              .grid(row=2, column=3, columnspan=1)
-
         tk.Button(root, text="x^y",   padx=33, pady=20, command=button_pwr)              .grid(row=1, column=2, columnspan=1)
         tk.Button(root, text="x√",    padx=36, pady=20, command=button_root)             .grid(row=1, column=1, columnspan=1)
         tk.Button(root, text="%",     padx=38, pady=20, command=button_mod)              .grid(row=1, column=0, columnspan=1)
